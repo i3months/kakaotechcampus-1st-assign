1c2fecaa8a3d369bc1dd89c0a97139a7e0ffdfa4
jmj <waysoleward01@naver.com>
2025-04-29 01:06:57 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const API_KEY = '174fbf45e0b90ad7e62c1841d5783bef';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const movieList = document.getElementById('movie-list');
+
+async function fetchPopularMovies() {
+  try {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`);
+    const data = await response.json();
+    console.log('인기 영화 데이터:', data.results);
+
+    renderMovies(data.results);
+  } catch (error) {
+    console.error('영화 데이터를 가져오는 중 오류 발생:', error);
+  }
+}
+
+function renderMovies(movies) {
+  movieList.innerHTML = ''; 
+
+  movies.forEach(movie => {
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
+
+    movieCard.innerHTML = `
+      <img src="${IMAGE_BASE_URL}${movie.poster_path}" alt="${movie.title}">
+      <div class="card-content">
+        <div class="title">${movie.title}</div>
+        <div class="rating">⭐ ${movie.vote_average}</div>
+      </div>
+    `;
+
+    movieList.appendChild(movieCard);
+  });
+}
+
+fetchPopularMovies();
